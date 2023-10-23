9ae33840d68967690a907a14f051403f39b99a05
glendymar <glendymarmatute@gmail.com>
2023-10-23 10:54:40 -0500

    mi primer componente en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Testimonio from './componentes/testimonio';
 import './App.css';
-
+//Archivo principal de JavaScript que crea un componente, la cual va a contener toda la estructura de nuestra aplicación
+//archivo principal que reder
+//componente funcional
+//En react lo que hacemos es que, ciertos componentes renderizan otros componentes, 
+//en este caso nuestro componente principal App esta generando a componente testimonio, mostrando así la estructura de nuestra aplicación.
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen algunos fans de harry potter:</h1>
+      <Testimonio
+      nombre="Anónimo"
+      pais="España"
+      imagen="fans1"
+      testimonio="Desde que abrí el primer libro de Harry Potter, supe que mi vida cambiaría para siempre. La magia de J.K. Rowling me envolvió y me llevó a un mundo de aventuras inolvidables. Cada página de la saga es como un hechizo que me transporta a Hogwarts. Los personajes se sienten tan reales que los considero amigos. Gracias a esta historia, mi infancia fue aún más mágica."/>
+      </div>
     </div>
   );
 }
--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -1,6 +1,22 @@
 import react from 'react';
-function testimonio() {
+import '../hojas-de-estilo/Testimonio.css';
+//Este es un componente funcional,es una función de javascript, este componente retorna un elemento jsx.
+function Testimonio(props) {
 return (
-
+  <><div className='contenedor-testimonio'>
+    <img className='imagen-testimonio'
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt='Foto de fans1'/>
+      <div className='contenedor-texto-testimonio'>
+      <p className='nombre-testimonio'>{props.nombre}</p>
+      <p className='país-testimonio'>{props.pais}</p>
+      <p className='texto-testimonio'>{props.testimonio}</p>
+      </div>
+  </div>
+  </>
 );
+
 }
+// En javascript existen dos tipos de exportación exportación por defecto y exportación nombrada.
+// Esta es exportación por defecto, que indica que sera el único elemento que estaremos exportando.
+export default Testimonio;
